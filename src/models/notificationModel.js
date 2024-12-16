24d2116da5db2292df5ada224909a1c88e3516c5
Julien Lisita <julien.lisita@gmail.com>
2024-12-16 23:52:01 +0100

    Create notification Model

---
+++ src/models/notificationModel.js
@@ -0,0 +1,52 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../config/database.js';
+
+const Notification = sequelize.define('Notification', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [[
+        'rappel_plat_planifie', 
+        'reception_message_recu', 
+        'recette_likee', 
+        'recette_notee', 
+        'recette_commentee',
+        'message_administrateur',
+        'information_generale'
+      ]],
+    },
+    comment: 'Type de notification : rappel_plat_planifie, reception_message_recu, etc.',
+  },
+  message: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [1, 255],
+        msg: 'Le message doit contenir entre 1 et 255 caractères.',
+      },
+    },
+  },
+  isRead: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+}, {
+  tableName: 'Notifications',
+  underscored: true,
+  timestamps: false, // Désactive les champs Sequelize automatiques
+});
+
+export default Notification;
