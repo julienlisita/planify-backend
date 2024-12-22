d8d9f52285c832e903a82ba69c96b8e103e26ac3
Julien Lisita <julien.lisita@gmail.com>
2024-12-22 13:28:12 +0100

    correct syntax in mock-users

--- src/data/mock-users.js
+++ src/data/mock-users.js
@@ -6,7 +6,7 @@ const mockUsers= [
       "password": "hashed_password_1", 
       "email": "admin@example.com",
       "avatar": null,
-      "role_id": 2
+      "roleId": 2
     },
     {
       "username": "johndoe",
@@ -15,7 +15,7 @@ const mockUsers= [
       "password": "hashed_password_2",
       "email": "johndoe@example.com",
       "avatar": "https://example.com/avatar1.png",
-      "role_id": 1
+      "roleId": 1
     },
     {
       "username": "janedoe",
@@ -24,7 +24,7 @@ const mockUsers= [
       "password": "hashed_password_3",
       "email": "janedoe@example.com",
       "avatar": null,
-      "role_id": 1
+      "roleId": 1
     },
     {
       "username": "author1",
@@ -33,7 +33,7 @@ const mockUsers= [
       "password": "hashed_password_4",
       "email": "marktwain@example.com",
       "avatar": "https://example.com/avatar2.png",
-      "role_id": 4
+      "roleId": 4
     },
     {
       "username": "superadmin",
@@ -42,7 +42,7 @@ const mockUsers= [
       "password": "hashed_password_5",
       "email": "superadmin@example.com",
       "avatar": "https://example.com/avatar3.png",
-      "role_id": 3
+      "roleId": 3
     }
   ]
 
