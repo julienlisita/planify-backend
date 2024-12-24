d6a77142db039b4392692acdca7b90b26fb9a347
Julien Lisita <julien.lisita@gmail.com>
2024-12-24 01:44:18 +0100

    create test suite for Like  model

---
+++ src/data/mock-likes.js
@@ -0,0 +1,24 @@
+const mockLikes = [
+    { id: 1, userId: 1, recipeId: 3, createdAt: new Date(), updatedAt: new Date() },
+    { id: 2, userId: 2, recipeId: 7, createdAt: new Date(), updatedAt: new Date() },
+    { id: 3, userId: 3, recipeId: 2, createdAt: new Date(), updatedAt: new Date() },
+    { id: 4, userId: 4, recipeId: 5, createdAt: new Date(), updatedAt: new Date() },
+    { id: 5, userId: 5, recipeId: 8, createdAt: new Date(), updatedAt: new Date() },
+    { id: 6, userId: 1, recipeId: 4, createdAt: new Date(), updatedAt: new Date() },
+    { id: 7, userId: 2, recipeId: 9, createdAt: new Date(), updatedAt: new Date() },
+    { id: 8, userId: 3, recipeId: 1, createdAt: new Date(), updatedAt: new Date() },
+    { id: 9, userId: 4, recipeId: 6, createdAt: new Date(), updatedAt: new Date() },
+    { id: 10, userId: 5, recipeId: 10, createdAt: new Date(), updatedAt: new Date() },
+    { id: 11, userId: 1, recipeId: 2, createdAt: new Date(), updatedAt: new Date() },
+    { id: 12, userId: 2, recipeId: 8, createdAt: new Date(), updatedAt: new Date() },
+    { id: 13, userId: 3, recipeId: 7, createdAt: new Date(), updatedAt: new Date() },
+    { id: 14, userId: 4, recipeId: 10, createdAt: new Date(), updatedAt: new Date() },
+    { id: 15, userId: 5, recipeId: 3, createdAt: new Date(), updatedAt: new Date() },
+    { id: 16, userId: 1, recipeId: 9, createdAt: new Date(), updatedAt: new Date() },
+    { id: 17, userId: 2, recipeId: 6, createdAt: new Date(), updatedAt: new Date() },
+    { id: 18, userId: 3, recipeId: 5, createdAt: new Date(), updatedAt: new Date() },
+    { id: 19, userId: 4, recipeId: 4, createdAt: new Date(), updatedAt: new Date() },
+    { id: 20, userId: 5, recipeId: 1, createdAt: new Date(), updatedAt: new Date() },
+  ];
+  
+  export default mockLikes;
