890f5a840c07dd8667c350ddcb44c1287c311778
Julien Lisita <julien.lisita@gmail.com>
2024-12-27 03:14:29 +0100

    syntax correction in mealPlanModel

--- src/models/mealPlanModel.js
+++ src/models/mealPlanModel.js
@@ -1,19 +1,26 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/database.js';
 
-const MealPLanModel = sequelize.define('MealPLanModel', {
+const MealPlan = sequelize.define('MealPLanModel', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
     },
-    slot: {
-        type: DataTypes.STRING, // Voir pour ENUM au lieu de STRING ou peut-être ajouter des validateurs pour plus de controle sur les valeurs
-        allowNull: false
+    note: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    meal_type: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            isIn: [[1, 2, 3, 4]], // Les valeurs autorisées pour meal_type
+    },
     },
     date: {
         type: DataTypes.DATE,
-        allowNull: false
+        allowNull: false,
     },
     created_at: {
         type: DataTypes.DATE,
@@ -49,19 +56,21 @@ const MealPLanModel = sequelize.define('MealPLanModel', {
 });
 
 // Fonction pour définir les associations
-mealPlan.associate = (models) => {
+MealPlan.associate = (models) => {
 
     // Un plan de repas appartient à un utilisateur
-    mealPlan.belongsTo(models.User, {
+    MealPlan.belongsTo(models.User, {
         foreignKey: 'userId',
         onDelete: 'CASCADE',
         onUpdate: 'CASCADE',
     });
 
     // Un plan de repas appartient à une recette
-    mealPlan.belongsTo(models.Recipe, {
-        through: MealPlanRecipe
+    MealPlan.belongsTo(models.Recipe, {
+        foreignKey: 'recipeId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
     });
 }
 
-export default MealPLanModel;
+export default MealPlan;
