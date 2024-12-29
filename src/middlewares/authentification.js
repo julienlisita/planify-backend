8cfa1d91965d3758d48b6e6ff0533b2e4c1df195
Julien Lisita <julien.lisita@gmail.com>
2024-12-29 23:53:22 +0100

    create isAdmin middleware

--- src/middlewares/authentification.js
+++ src/middlewares/authentification.js
@@ -30,4 +30,38 @@ const authenticateAndAuthorize = async (req, res, next) => {
     }
 };
 
-export default authenticateAndAuthorize;
+const isAdmin = async (req, res, next) => {
+    try {
+      // Récupérer le token depuis les en-têtes de la requête
+      const token = req.headers.authorization?.split(' ')[1]; // On suppose que le token est dans le format "Bearer <token>"
+  
+      // Si aucun token n'est fourni, renvoyer une erreur
+      if (!token) {
+        return res.status(401).json({ error: 'Accès non autorisé, token manquant' });
+      }
+  
+      // Vérifier le token avec la clé secrète
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);  // Décodage du token avec la clé secrète
+  
+      // Récupérer l'utilisateur depuis la base de données à l'aide de l'ID contenu dans le token
+      const user = await User.findByPk(decoded.id);
+  
+      if (!user) {
+        return res.status(404).json({ error: 'Utilisateur non trouvé' });
+      }
+  
+      // Vérifier si l'utilisateur a le rôle 'admin'
+      if (user.role !== 'admin' && user.role !== 'superAdmin') {
+        return res.status(403).json({ error: 'Accès réservé aux administrateurs' });
+      }
+  
+      // Si l'utilisateur est admin, passer au middleware suivant
+      req.user = user;  // Ajouter l'utilisateur dans la requête pour l'utiliser dans les autres middlewares ou contrôleurs
+      next();
+    } catch (error) {
+      console.error('Erreur dans le middleware isAdmin:', error);
+      res.status(500).json({ error: 'Erreur serveur lors de la vérification du rôle' });
+    }
+  };
+
+export default {authenticateAndAuthorize, isAdmin};
