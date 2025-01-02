5a5b540ba646235c18c0623951e3905ee87151a1
Julien Lisita <julien.lisita@gmail.com>
2025-01-02 23:26:38 +0100

    feat: add test data to mockUsers

--- src/data/mock-users.js
+++ src/data/mock-users.js
@@ -1,37 +1,73 @@
 const mockUsers= [
     {
       "username": "superAdmin",
-      "firstname": "Alice",
-      "lastname": "Martin",
+      "firstname": "Jonathan",
+      "lastname": "Boyer",
       "password": "hashed_password_1", 
-      "email": "superadmin@example.com",
+      "email": "grafikart@example.com",
       "avatar": null,
       "roleId": 1
     },
     {
-      "username": "Admin",
+      "username": "superAdmin2",
+      "firstname": "Yoann",
+      "lastname": "Kipfer",
+      "password": "hashed_password_2", 
+      "email": "kipdev@example.com",
+      "avatar": null,
+      "roleId": 1
+    },
+    {
+      "username": "Admin1",
       "firstname": "John",
       "lastname": "Doe",
-      "password": "hashed_password_2",
-      "email": "admin@example.com",
+      "password": "hashed_password_3",
+      "email": "joahndoe@example.com",
       "avatar": "https://example.com/avatar1.png",
       "roleId": 2
     },
     {
-      "username": "author1",
-      "firstname": "Julien",
-      "lastname": "Lisita",
-      "password": "hashed_password_3",
-      "email": "julienlisita@example.com",
-      "avatar": "https://example.com/avatar1.png",
-      "roleId": 3
-    },
-    {
-      "username": "author2",
+      "username": "Admin2",
       "firstname": "Jane",
       "lastname": "Doe",
       "password": "hashed_password_4",
       "email": "janedoe@example.com",
+      "avatar": "https://example.com/avatar2.png",
+      "roleId": 2
+    },
+    {
+      "username": "Admin3",
+      "firstname": "Julien",
+      "lastname": "Lisita",
+      "password": "hashed_password_5",
+      "email": "julienlisita@example.com",
+      "avatar": "https://example.com/avatar3.png",
+      "roleId": 2
+    },
+    {
+      "username": "Admin4",
+      "firstname": "Jean-Jacques",
+      "lastname": "Gilardi",
+      "password": "hashed_password_6",
+      "email": "jjgilardi@example.com",
+      "avatar": "https://example.com/avatar4.png",
+      "roleId": 2
+    },
+    {
+      "username": "author1",
+      "firstname": "David",
+      "lastname": "Robert",
+      "password": "hashed_password_7",
+      "email": "drobert@example.com",
+      "avatar": null,
+      "roleId": 3
+    },
+    {
+      "username": "author2",
+      "firstname": "Paul",
+      "lastname": "Doazan",
+      "password": "hashed_password_8",
+      "email": "pdoazan@example.com",
       "avatar": null,
       "roleId": 3
     },
@@ -39,18 +75,63 @@ const mockUsers= [
       "username": "member1",
       "firstname": "Mark",
       "lastname": "Twain",
-      "password": "hashed_password_5",
+      "password": "hashed_password_9",
       "email": "marktwain@example.com",
-      "avatar": "https://example.com/avatar2.png",
+      "avatar": "https://example.com/avatar5.png",
       "roleId": 4
     },
     {
       "username": "Member2",
       "firstname": "Emily",
       "lastname": "Clark",
-      "password": "hashed_password_6",
-      "email": "Member2@example.com",
-      "avatar": "https://example.com/avatar3.png",
+      "password": "hashed_password_10",
+      "email": "eclark@example.com",
+      "avatar": "https://example.com/avatar6.png",
+      "roleId": 4
+    },
+    {
+      "username": "Member3",
+      "firstname": "Emma",
+      "lastname": "Egou",
+      "password": "hashed_password_10",
+      "email": "emegou@example.com",
+      "avatar": "https://example.com/avatar7.png",
+      "roleId": 4
+    },
+    {
+      "username": "Member4",
+      "firstname": "Laura",
+      "lastname": "Dos Santos",
+      "password": "hashed_password_10",
+      "email": "lolods@example.com",
+      "avatar": "https://example.com/avatar8.png",
+      "roleId": 4
+    },
+    {
+      "username": "Member5",
+      "firstname": "Anna",
+      "lastname": "Dos Santos",
+      "password": "hashed_password_10",
+      "email": "annads@example.com",
+      "avatar": "https://example.com/avatar9.png",
+      "roleId": 4
+    },
+    {
+      "username": "Member6",
+      "firstname": "Dominique",
+      "lastname": "Lisita",
+      "password": "hashed_password_10",
+      "email": "dlisita@example.com",
+      "avatar": "https://example.com/avatar10.png",
+      "roleId": 4
+    },
+    {
+      "username": "Member7",
+      "firstname": "Christiane",
+      "lastname": "Lisita",
+      "password": "hashed_password_10",
+      "email": "clisita@example.com",
+      "avatar": "https://example.com/avatar11.png",
       "roleId": 4
     }
   ]
